da58d6bf4037b79d9c29701e659361b11a02d5c9
EC2 Default User <ec2-user@ip-172-31-28-104.ec2.internal>
2020-10-11 23:31:31 +0000

    bot can now translate! using google api

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import os
 import flask
 import flask_socketio
+from os.path import join, dirname
+from dotenv import load_dotenv
+from google.cloud import translate_v2 as translate
+import json
+
+dotenv_path = join(dirname(__file__), 'pro2.env')
+load_dotenv(dotenv_path)
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']="/home/ec2-user/environment/lect8-react/google.json"
+
 
 app = flask.Flask(__name__)
 
@@ -39,8 +49,10 @@ def on_new_number(data):
     name=data['name']
     nameCounter=0
     botMessage=""
+    translate_client = translate.Client()
+    output = translate_client.translate( message, target_language='ja')
     if message=="hi bot":
-        botMessage="bot: yo whats up"
+        botMessage="bot: " + output['translatedText']
     if name not in nameList:
         nameList.append(name)
         nameCounter+=1
