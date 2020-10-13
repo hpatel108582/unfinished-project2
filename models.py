53f9a6ca3211ff1f8d502251b51bbe4e9c386d48
EC2 Default User <ec2-user@ip-172-31-28-104.ec2.internal>
2020-10-13 23:50:25 +0000

    added table models python file

---
+++ models.py
@@ -0,0 +1,13 @@
+import flask_sqlalchemy
+from app import db
+
+
+class Messanger(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(120))
+    
+    def __init__(self, a):
+        self.message = a
+        
+    def __repr__(self):
+        return '<User message: %s>' % self.message
